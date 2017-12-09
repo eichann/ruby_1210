c26c9a42c3b75e555d0517d530ad6449cfeb43aa
eichann <k.abe.in401@gmail.com>
2017-12-10 04:57:00 +0900

    Gearクラスを作成

--- gear.rb
+++ gear.rb
@@ -1,12 +1,30 @@
 #ギア比を出すプログラム
 
-chainring = 52
-cog = 11
-ratio = chainring / cog.to_f
-puts ratio
+# chainring = 52
+# cog = 11
+# ratio = chainring / cog.to_f
+# puts ratio
+# 
+# 
+# chainring = 30
+# cog = 27
+# ratio = chainring / cog.to_f
+# puts ratio
 
+class Gear
+  attr_reader :chainring, :cog
+  
+  def initialize(chainring, cog)
+    @chainring = chainring
+    @cog = cog
+  end
+  
+  def ratio
+    chainring / cog.to_f
+  end
+  
+end
 
-chainring = 30
-cog = 27
-ratio = chainring / cog.to_f
-puts ratio
+puts Gear.new(52, 11).ratio
+
+puts Gear.new(30, 27).ratio
