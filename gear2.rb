cada387cac46e0e16004a0020ab29e69be222033
eichann <k.abe.in401@gmail.com>
2017-12-10 09:46:33 +0900

    create gear2

---
+++ gear2.rb
@@ -0,0 +1,21 @@
+class Gear
+  attr_reader :chainring, :cog, :rim, :tire
+  
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog = cog
+    @rim = rim
+    @tire = tire
+  end
+  
+  def ratio
+    chainring / cog.to_f
+  end
+  
+  def gear_inches
+    ratio * Wheel.new(rim, tire).diameter
+  end
+  
+end
+
+Gear.new(52, 11, 26, 1.5).gear_inches
