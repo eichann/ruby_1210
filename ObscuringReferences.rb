a5ed890fd765b5ef8b3d956411cf188861b3cf56
eichann <k.abe.in401@gmail.com>
2017-12-10 09:16:08 +0900

    Use struct class

--- ObscuringReferences.rb
+++ ObscuringReferences.rb
@@ -10,3 +10,21 @@ class ObscuringReferences
     data.collect ( |cell| cell[0] + (cell[1] * 2) )
   end
 end
+
+class RevealingReferences
+  attr_reader :wheels
+  
+  def initialize(data)
+    @wheels = wheelify(data)
+  end
+  
+  def diameters
+    wheels.collect ( |wheel| wheel.rim + (wheel.tire * 2) )
+  end
+  
+  Wheel = Struct.new(:rim, :tire)
+  def wheelify(data)
+    data.collect ( |cell| Wheel.new(cell[0], cell[1]) )
+  end
+end
+
